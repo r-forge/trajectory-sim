b80bd8c9b9c213b9dd28c0b4ede3a91e082bd527
sijben (Stef Sijben) <Stef.Sijben@ruhr-uni-bochum.de>
2014-07-17 10:41:51 +0000

    Implemented discrete Fréchet distance
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/trajectory-sim@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/discrete_Frechet.R
@@ -0,0 +1,27 @@
+## Compute trajectory similarities according to the discrete Fréchet distance, under a convex distance measure.
+
+"discrete.frechet" <- function(trajectories, dp=euclidian) {
+	trajectory.similarity(trajectories, implementation=discrete.frechet.pairwise, dp=dp, symmetric=TRUE, diagonal=0)
+}
+
+"discrete.frechet.pairwise" <- function(T1, T2, dp=euclidian) {
+	traj.sim.dp(T1, T2, .discrete.frechet.step.fun, dp)
+}
+
+## DP step function for discrete Fréchet
+".discrete.frechet.step.fun" <- function(i, j, prev=list()) {
+	if (length(prev) == 0) {
+		## Lower left corner of table
+		new("ts.dp.entry",
+				value=dp(T1[i,], T2[j,]),
+				pred=NULL)
+	} else {
+		# Select the predecessor with the smallest d_dF so far
+		pm <- which.min(sapply(prev, function(p) { p@value }))
+		p <- prev[[pm]]
+		new("ts.dp.entry",
+				value=max(p@value, dp(T1[i,], T2[j,])),
+				pred=names(pm))
+	}
+}
+
